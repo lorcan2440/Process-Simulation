0dae930a913717aaba98de00991b45dd10758b53
Lorcan Nicholls <ln356@cam.ac.uk>
2023-08-12 19:12:15 +0100

    first commit

---
+++ Chemical-Mixing-Plant/cstr_sim.py
@@ -0,0 +1,213 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+from scipy.misc import derivative
+from matplotlib import animation as anim, pyplot as plt
+import matplotlib as mpl
+from matplotlib.widgets import Slider
+from CoolProp.CoolProp import PropsSI
+
+###############
+#### SETUP ####
+###############
+
+## application constants
+
+FFMPEG_PATH = r'C:\LibsAndApps\ffmpeg-build\bin\ffmpeg.exe'
+MPL_STYLESHEET = r'C:\LibsAndApps\Python config files\proplot_style.mplstyle'
+
+## solver settings
+
+INITIAL_T = 0                         # simulation start time
+MAX_STEP_T = 0.5                     # maximum integration step size
+ANIM_STEP_T = 5                     # update frame every interval
+SLIDING_WINDOW_T = 60                 # start moving along with the graph after this time
+
+AXIS_TEMP_MIN = 300
+AXIS_TEMP_RANGE = 100
+AXIS_CONC_MIN = 0
+AXIS_CONC_RANGE = 0.3
+
+## simulation settings
+
+# chemistry and geometry (fixed)
+A = 3.274e7         # Arrhenius constant in rate equation               [mol^-1 dm^3 s^-1]
+E_A = 5.0e4         # activation energy for reaction in rate equation   [J]
+DH = -2.632e4       # enthalpy change for reaction                      [J mol^-1]
+R = 8.314           # gas constant                                      [J mol^-1 K^-1]
+V = 120             # volume of vessel                                  [dm^3 = litres]
+Q = 20              # total flow rate                                   [dm^3 s^-1]
+UA = 880 * 3.95     # steam heater overall HTC * surface area           [W K^-1]
+
+# functions of solvent and chemistry
+RHO = lambda T: 1e-3 * PropsSI('D', 'T|liquid', T, 'P', 1e5, 'Water')   # solvent density
+D_RHO_DT = lambda T: derivative(RHO, T, 1e-3)                           # [kg dm^-3]
+C_P = lambda T: PropsSI('C', 'T|liquid', T, 'P', 1e5, 'Water')          # solvent SHC
+D_CP_DT = lambda T: derivative(C_P, T, 1e-3)                            # [J kg^-1 K^-1]
+K = lambda T: A * np.exp(-E_A / (R * T))                                # rate constant
+                                                                        # [mol^-1 dm^3 s^-1]
+
+
+# manipulated vars (initial values)
+Q_A = 10            # inflow rate of A                                  [dm^3 s^-1]
+C_A1 = 1.5          # concentration of A at inflow                      [mol dm^-3]
+C_B1 = 1.8          # concentration of B at inflow                      [mol dm^-3]
+T_A1 = 300          # temperature of inflow of A                        [K]
+T_B1 = 320          # temperature of inflow of A                        [K]
+T_S = 973           # steam heater temperature                          [K]
+T_2_INIT = 300      # initial solvent temperature                       [K]
+
+# constrained vars
+Q_B = Q - Q_A                                       # inflow rate of B  [dm^3 s^-1]
+WINDOW = int(ANIM_STEP_T / MAX_STEP_T)              # number of integrations per frame
+INITIAL_VALUES = np.array([0, 0, 0, T_2_INIT])      # initial c_A, c_B, c_C, T in vessel
+DEP_VAR_NAMES = ('$c_A$', '$c_B$', '$c_C$', '$T$')  # labels for dependent variables
+history = {
+    'last_vals': INITIAL_VALUES,
+    'all_t': np.array([INITIAL_T]),
+    'all_y_vec': np.array([[y_val] for y_val in INITIAL_VALUES])
+    }
+
+## apply settings
+
+vid_writer = anim.FFMpegWriter(fps=30, codec='libx264', bitrate=-1)
+
+
+## Sliders
+
+def init_graph():
+
+    mpl.rcParams['animation.ffmpeg_path'] = FFMPEG_PATH
+    plt.rcParams["figure.autolayout"] = True
+    plt.style.use(MPL_STYLESHEET)
+
+
+###############
+#### MODEL ####
+###############
+
+def ode_system(t: float, y: np.ndarray,
+        const_rho: bool = False, const_shc: bool = False, const_rate: bool = False) -> np.ndarray:
+
+    # get dependent vars
+    c_A2, c_B2, c_C2, T_2 = y
+
+    # get slider values
+    C_A1 = slider_c_A.val
+    C_B1 = slider_c_B.val
+
+    # set temperature-dependent quantities
+    k = 0.05584 if const_rate else K(T_2)        # rate constant            [mol^-1 dm^3 s^-1]
+    rho = 1.000 if const_rho else RHO(T_2)       # density                  [kg dm^-3]
+    c_p = 4200 if const_shc else C_P(T_2)        # isobaric SHC             [J kg^-1 K^-1]
+    dcp_dt = 0 if const_shc else D_CP_DT(T_2)    # temperature derivative   [J kg^-1 K^-2]
+    drho_dt = 0 if const_rho else D_RHO_DT(T_2)  # temperature derivative   [kg dm^-3 K^-1]
+    energy_balance_numer = rho * c_p * (Q_A * T_A1 + Q_B * T_B1 - Q * T_2) \
+        - k * V * DH * c_A2 * c_B2 + UA * (T_S - T_2)                     # [J s^-1]
+    energy_balance_denom = rho * V * (c_p + T_2 * dcp_dt) \
+        + c_p * V * drho_dt                                               # [J K^-1]
+    
+    # form system of differential equations
+    return np.array([
+        C_A1 * Q_A / V - c_A2 * Q / V - k * c_A2 * c_B2,
+        C_B1 * Q_B / V - c_B2 * Q / V - k * c_A2 * c_B2,
+        -c_C2 * Q / V + k * c_A2 * c_B2,
+        energy_balance_numer / energy_balance_denom
+    ])
+
+
+###################
+#### ANIMATION ####
+###################
+
+def animate(frame: int, history: dict[str, np.ndarray]) -> None:
+
+    # clear axes and set graphical options
+    ax_c.cla()
+    ax_T.cla()
+    ax_c.set_title(r'Reaction: $ A + B \rightarrow C $')
+    ax_T.set_xlabel(r'time / s')
+    ax_c.set_ylabel(r'concentration / $ mol \ dm^{-3} $')
+    ax_T.set_ylabel(r'temperature / $ K $')
+    
+    ax_c.tick_params(axis='both', which='both', labelsize=7, labelbottom=True)
+    ax_T.tick_params(axis='both', which='both', labelsize=7)
+
+    # the new values of t to be plotted in this frame
+    t_range = np.array([INITIAL_T + MAX_STEP_T * frame * WINDOW, \
+                        INITIAL_T + MAX_STEP_T * (frame + 1) * WINDOW])
+
+    # set first values for this range
+    last_vals, all_t, all_y_vec = history['last_vals'], history['all_t'], history['all_y_vec']
+    last_vals = all_y_vec[:, -1] if frame != 0 else INITIAL_VALUES
+
+    # solve system over these t bounds
+    sol = solve_ivp(ode_system, t_range, last_vals, args=(True, True, False),
+        method='LSODA', max_step=MAX_STEP_T)  # solve the system over these t
+    
+    # record values up to this latest point
+    all_t = np.concatenate((all_t, sol.t))
+    all_y_vec = np.concatenate((all_y_vec, sol.y), axis=1)
+
+    # chop down lists and set new axis limits
+    if SLIDING_WINDOW_T is not None:
+        new_cutoff = all_t[-1] - SLIDING_WINDOW_T
+        all_t = all_t[all_t >= new_cutoff]
+        all_y_vec = all_y_vec[:, -1 * len(all_t):]
+
+        # set x axis range
+        _, t_max = ax_T.get_xlim()
+        if all_t[-1] < INITIAL_T + SLIDING_WINDOW_T:
+            ax_c.set_xlim(INITIAL_T, INITIAL_T + SLIDING_WINDOW_T)
+            ax_T.set_xlim(INITIAL_T, INITIAL_T + SLIDING_WINDOW_T)
+        elif all_t[-1] >= t_max:
+            ax_c.set_xlim(all_t[-1] - SLIDING_WINDOW_T, all_t[-1])
+            ax_T.set_xlim(all_t[-1] - SLIDING_WINDOW_T, all_t[-1])
+        
+        # set y axis range (fix T)
+        ax_T.set_ylim(AXIS_TEMP_MIN, AXIS_TEMP_MIN + AXIS_TEMP_RANGE)
+
+
+    # update the record of the previous values, passed into the next animation frame
+    history.update({'last_vals': last_vals, 'all_t': all_t, 'all_y_vec': all_y_vec})
+
+    # plot lines up to the latest point
+    for i, y_vec in enumerate(all_y_vec):
+        if 0 <= i <= 2:  # concentration
+            ax_c.plot(all_t, y_vec, label=DEP_VAR_NAMES[i])
+            ax_c.legend(loc='lower left')
+        elif i == 3:     # temperature
+            ax_T.plot(all_t, y_vec, label=DEP_VAR_NAMES[i])
+            ax_T.legend(loc='lower left')
+
+
+if __name__ == '__main__':
+
+    # set up figure and axes
+    fig = plt.gcf()
+    ax_c = plt.subplot(2, 2, 1)
+    ax_T = plt.subplot(2, 2, 3)
+    ax_img = plt.subplot(1, 2, 2)
+    plt.subplots_adjust(bottom=0.20)
+
+    # set up sliders
+    ax_slider_c_A = plt.axes([0.125, 0.1, 0.5, 0.02], facecolor="lightgrey")
+    ax_slider_c_B = plt.axes([0.125, 0.06, 0.5, 0.02], facecolor="lightgrey")
+    slider_c_A = Slider(ax_slider_c_A, '$ c_A^{(in)} $', 0, 5, valinit=C_A1,
+        valfmt=r'%0.3f $ mol \ dm^{-3} $')
+    slider_c_B = Slider(ax_slider_c_B, '$ c_B^{(in)} $', 0, 5, valinit=C_B1,
+        valfmt=r'%0.3f $ mol \ dm^{-3} $')
+    ax_slider_c_B.add_artist(ax_slider_c_B.xaxis)
+    ax_slider_c_B.set_xticks(np.arange(0, 5.01, 1))
+
+    # add still image on right
+    ax_img.imshow(plt.imread(r'Chemical-Mixing-Plant/cstr_pic.png'))
+    ax_img.set_axis_off()
+
+    # render animation
+    ani = anim.FuncAnimation(fig, animate, fargs=(history,), init_func=init_graph, \
+                             interval=30, save_count=100)
+    plt.show()
+
+    print('Making video...')
+    ani.save('Chemical-Mixing-Plant/Simulation.mp4', writer=vid_writer)
+    print('Finished!')
---
+++ Queueing/main.py
@@ -0,0 +1,229 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.ticker as plticker
+
+import simpy
+import random
+import math
+import itertools
+
+
+class MMcNK_Queue:
+    def __init__(self, lambda_: float = 1, mu: float = 1, c: int = 1,
+                 N: int = float('inf'), K: int = float('inf'), env: simpy.Environment = None):     
+        '''
+        Define an M/M/c/N/K queue, a continuous-time Markov chain (CTMC) and stochastic process
+        in which 'customers' from a 'population' arrive and are served acoording to a Poisson point process,
+        with options for a finite queue size and finite population size.
+        
+        #### Arguments
+        
+        - `lambda_` (float, default = 1): the rate parameter for customers attempting to
+        enter the system.
+        - `mu` (float, default = 1): the rate parameter for each customer leaving a service node.
+        - `c` (int, default = 1): the number of service nodes.
+        - `N` (int, default = float('inf')): the maximum possible size of the system, so
+        that the maximum queue length is `N - c`. If unset, the size is unlimited.
+        - `K` (int, default = float('inf')): the maximum possible size of the population.
+        If `K = N` then the queue is closed, so that customers leaving the service node
+        immediately may wish to enter the queue again.
+        - `env` (simpy.Environment, default = None): a `SimPy` environment instance to use
+        in this simulation.
+        ''' 
+        self.env = simpy.Environment() if env is None else env
+        self.server = simpy.Resource(self.env, capacity=c)
+        self.queue = simpy.Store(self.env) if N == float('inf') else simpy.Store(self.env, capacity=N - c)
+        self.lamda = lambda_
+        self.mu = mu
+        self.c = c
+        self.K = K
+        self.N = N
+        self.num_people_served = 0  # people who entered the queue, went to a server, and left
+        self.num_people_blocked_data = [(0, 0)]  # people who tried to enter but found a full queue, so did not get served never got served
+        self.results = {}
+
+    def run_until_time(self, runtime: float):
+        '''
+        Run the simulation of the queue for a set simulation end time.
+        
+        #### Arguments
+        
+        `runtime` (float): amount of time after which to stop the simulation, measuring in the simulated world.
+
+        #### Returns
+
+        `self.results` (dict[int: list[float]]): information about all customers who entered the system.
+        The key is the index of the customer.
+        The value is `[time_of_entering_queue, time_moved_to_counter, time_left_system]`.
+        These entries are `None` if the customer did not make it to the counter
+        or leave the system by the time the simulation ended.
+        '''
+        self.env.process(self.generate_arrivals_forever())
+        self.env.run(until=runtime)
+        return self.results
+
+    def generate_arrivals_forever(self, end_condition_customers: int = float('inf')):
+        id_num = 0
+        while True:
+            # wait for the next customer
+            inter_arrival_time = random.expovariate(self.lamda)
+            yield self.env.timeout(inter_arrival_time)  # wait a random entry time
+
+            # check there are some customers in the population - if not, cycle
+            is_people_in_population = (self.K - len(self.queue.items) + len(self.server.users) > 0)
+            if not is_people_in_population:
+                continue
+
+            # a wild customer appears! Can they fit in the queue?
+            is_free_queue_space = (len(self.queue.items) < self.queue.capacity)
+            if is_free_queue_space:
+                # admit to queue and enter process for them
+                self.results.update({id_num: [self.env.now, None, None, True]})
+                self.queue.put(id_num)
+                self.env.process(self.process_customer(id_num))
+            else:
+                # record their attempted entry but do not enter the queue
+                self.num_people_blocked_data.append((self.env.now, self.num_people_blocked_data[-1][1] + 1))
+                self.results.update({id_num: [self.env.now, None, None, False]})
+
+            # end simulation if end condition met
+            if id_num >= end_condition_customers:
+                break
+            id_num += 1
+
+    def process_customer(self, id_num):
+        # serve a customer at the front of the queue
+        with self.server.request() as request:
+            yield request
+            _id_moved = self.queue.get()  # remove this customer from the queue
+            self.results[id_num][1] = self.env.now  # time moved to the counter
+            service_time = random.expovariate(self.mu)
+            yield self.env.timeout(service_time)
+            self.results[id_num][2] = self.env.now  # time left the system
+            self.num_people_served += 1
+
+    def get_statistics(self):
+
+        # calculate wait times per customer
+        system_wait_times, queue_wait_times, service_times = zip(*[
+            [val[2] - val[0], val[1] - val[0], val[2] - val[1]] for _, val in self.results.items() \
+                if all([x is not None for x in val])])
+        mean_queue_wait_time = sum(queue_wait_times) / len(system_wait_times)
+
+        # calculate queue lengths
+        join_times = [(1, val) for val in [data[0] for data in self.results.values() if data[3]]]
+        leave_times = [(-1, val) for val in [data[1] for data in self.results.values() if data[3]]]
+        leave_times = list(filter(lambda x: x[1] is not None, leave_times))
+        change_times = sorted(join_times + leave_times, key=lambda x: x[1])
+        queue_changes = [c[0] for c in change_times]
+        queue_cumulative_changes = itertools.accumulate(queue_changes)
+        queue_lengths = [(0, 0)] + [(total, time) for (total, (_, time)) in \
+                                    zip(queue_cumulative_changes, change_times)]
+
+        # get time weighted average queue length
+        time_sum = 0
+        for x, y in zip(queue_lengths[:-1], queue_lengths[1:]):
+            time_sum += x[0] * (y[1] - x[1])
+        mean_queue_length = time_sum / queue_lengths[-1][1]
+
+        # get theoretical stats
+        expected_queue_time, expected_queue_length = self.get_theoretical_statistics()
+
+        print(f'Queue length: Simulated {mean_queue_length}, Theoretical {expected_queue_length}, '
+              f'Diff {100 * (1 - mean_queue_length / expected_queue_length):.4f}%')
+        print(f'Queue time: Simulated {mean_queue_wait_time}, Theoretical {expected_queue_time}, '
+              f'Diff {100 * (1 - mean_queue_wait_time / expected_queue_time):.4f}%')
+    
+    def get_theoretical_statistics(self):
+        '''
+        Only valid for M/M/c/N queue (K is infinite)
+        '''
+        rho = self.lamda / (self.c * self.mu)  # server utilisation
+        pi_0 = (1 + sum([(self.c * rho) ** n / math.factorial(n) for n in range(1, self.c + 1)])
+                + (self.c * rho) ** self.c / math.factorial(self.c) * sum([
+                    rho ** (n - self.c) for n in range(self.c + 1, self.N + 1)
+                ])) ** -1
+        pi_n = lambda n: pi_0 * (self.c * rho) ** n / math.factorial(n) if 0 <= n < self.c \
+            else pi_0 * (self.c * rho) ** n * self.c ** (self.c - n) / math.factorial(self.c)
+        pi_N = pi_0 * (self.c * rho) ** self.N / (math.factorial(self.c) * self.c ** (self.N - self.c))
+        E_queue_length = pi_0 * (self.c * rho) ** self.c * rho / (math.factorial(self.c) * (1 - rho) ** 2) * (
+            1 - rho ** (self.N - self.c) - (self.N - self.c) * (1 - rho) * rho ** (self.N - self.c)
+        )
+        lambda_eff = self.lamda * (1 - pi_N)
+        E_queue_time = E_queue_length / lambda_eff  # Little's lemma
+
+        return E_queue_time, E_queue_length
+
+    def animate_queue_length(self, window_size: float = 1):
+        fig, ax1 = plt.subplots(figsize=(10, 6))
+        ax2 = ax1.twinx()
+
+        self.event_timepoints = [] 
+        self.num_people_served_data = []
+        self.queue_length_data = []
+        self.server_length_data = []
+
+        def init():
+            pass
+
+        def update(frame):
+
+            # process up to next event
+            self.env.step()
+
+            # prepare axes
+            ax1.cla()
+            ax1.set_xlabel("Time (minutes)")
+            ax1.set_ylabel("Number of People")
+            ax1.set_xlim(max(0, self.env.now - window_size), max(window_size, self.env.now))
+            ax1.yaxis.set_major_locator(plticker.MultipleLocator(base=1.0))
+
+            ax2.cla()
+            ax2.set_ylabel("Cumulative Number of People")
+            ax2.yaxis.set_label_position("right")
+            ax2.yaxis.tick_right()
+            ax2.grid(False)
+            ax2.minorticks_off()
+
+            # get current state and update history
+            queue_length = len(self.queue.items)
+            server_length = len(self.server.users)
+            self.queue_length_data.append((self.env.now, queue_length))  # Store (time, queue_length)
+            self.server_length_data.append((self.env.now, server_length))  # Store (time, server_length)
+            self.num_people_served_data.append((self.env.now, self.num_people_served))
+            self.num_people_blocked_data.append((self.env.now, self.num_people_blocked_data[-1][1]))
+            self.event_timepoints.append(self.env.now)
+
+            # add data points before step changes to make to the plot
+            # look like rectangles rather than lines joining points
+            if frame != 0 and self.queue_length_data[-1][1] != self.queue_length_data[-2][1]:
+                self.queue_length_data.insert(-1, (self.env.now, self.queue_length_data[-2][1]))
+            if frame != 0 and self.server_length_data[-1][1] != self.server_length_data[-2][1]:
+                self.server_length_data.insert(-1, (self.env.now, self.server_length_data[-2][1]))
+            if frame != 0 and self.num_people_served_data[-1][1] != self.num_people_served_data[-2][1]:
+                self.num_people_served_data.insert(-1, (self.env.now, self.num_people_served_data[-2][1]))
+            if frame != 0 and self.num_people_blocked_data[-1][1] != self.num_people_blocked_data[-2][1]:
+                self.num_people_blocked_data.insert(-1, (self.env.now, self.num_people_blocked_data[-2][1]))
+
+            # plot
+            ax1.plot(*zip(*self.queue_length_data), label='In the Queue')
+            ax1.plot(*zip(*self.server_length_data), color='black', alpha=0.3, label='Being Served')
+            ax2.plot(*zip(*self.num_people_served_data), color='green', linestyle=':', alpha=0.3, label='Total Served')
+            ax2.plot(*zip(*self.num_people_blocked_data), color='red', linestyle='--', alpha=0.3, label='Total Blocked')
+            ax1.legend(loc='upper left')
+            ax2.legend(loc='upper center')
+
+        ani = FuncAnimation(fig, update, init_func=init, interval=30, blit=False)
+        plt.show()
+
+def main():
+    # Simulation
+    q = MMcNK_Queue(lambda_=5, mu=3, c=2, N=7)
+    q.env.process(q.generate_arrivals_forever())  # Start customer arrival process
+
+    # Animate queue length
+    q.animate_queue_length(window_size=3)
+    q.get_statistics()
+
+if __name__ == '__main__':
+    main()
